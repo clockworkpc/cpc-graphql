9f627a9a9e9845f3205d6cc4d7ae3abf6b271826
Alexander Garber <clockworkpc@gmail.com>
2020-03-04 08:18:36 +1100

    section 2 mutations

--- index.js
+++ index.js
@@ -8,12 +8,44 @@ app.get( '/', ( req, res ) => {
   res.send( 'GraphQL is amazing!' );
 } );
 
-const root = { hello: () => "Hi, I'm Manny" };
+class Friend {
+  constructor( id, { firstName, lastName, sex, language, email } ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.sex = sex;
+    this.language = language;
+    this.email = email;
+  }
+}
+
+const friendDatabase = {};
+
+const root = {
+  friend: () => {
+    return {
+      "id": 28718992,
+      "firstName": "Manny",
+      "lastName": "Henri",
+      "sex": "Male",
+      "language": "English",
+      "emails": [
+        { email: "manny@me.com" },
+        { email: "alex@me.com" }
+      ],
+    }
+  },
+  createFriend: ( { input } ) => {
+    let id = require( 'crypto' ).randomBytes( 10 ).toString( 'hex' );
+    friendDatabase[ id ] = input;
+    return new Friend( id, input );
+  }
+};
 
 app.use( '/graphql', graphqlHTTP( {
   schema: schema,
   rootValue: root,
   graphiql: true,
-} ) )
+} ) );
 
 app.listen( 8080, () => console.log( 'Running server on port localhost:8080/graphql' ) );
