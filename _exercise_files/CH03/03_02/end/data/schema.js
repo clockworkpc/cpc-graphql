30642c93f9bf62f66b565a87dce02a61922bea75
Alexander Garber <clockworkpc@gmail.com>
2020-03-05 17:54:29 +1100

    mongoose

---
+++ _exercise_files/CH03/03_02/end/data/schema.js
@@ -0,0 +1,53 @@
+import { resolvers } from './resolvers';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const typeDefs = `
+    type Friend {
+        id: ID
+        firstName: String
+        lastName: String
+        sex: Sex
+        age: Int
+        language: String
+        email: String
+        contacts: [Contact]
+    }
+
+    type Contact {
+        firstName: String
+        lastName: String
+    }
+
+    enum Sex {
+        MALE
+        FEMALE
+    }
+
+    type Query {
+        getFriend(id: ID): Friend
+    }
+
+    input FriendInput {
+        id: ID
+        firstName: String!
+        lastName: String
+        sex: Sex
+        age: Int
+        language: String
+        email: String
+        contacts: [ContactInput]
+    }
+
+    input ContactInput {
+        firstName: String
+        lastName: String
+    }
+
+    type Mutation {
+        createFriend(input: FriendInput): Friend
+    }
+`;
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export { schema };
--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import schema from './schema';
-import resolvers from './resolvers';
+import { schema } from './data/schema';
 
 const app = express();
 
-app.get( '/', ( req, res ) => {
-  res.send( 'GraphQL is amazing!' );
-} );
+app.get('/', (req, res) => {
+  res.send('GraphQL is amazing!');
+});
 
-const root = resolvers;
-
-app.use( '/graphql', graphqlHTTP( {
+app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
-} ) );
+}));
 
-app.listen( 8080, () => console.log( 'Running server on port localhost:8080/graphql' ) );
+app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
