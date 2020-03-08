5f9ebaaf4147c73fbd8270bbebb1d259ed35bde5
alexander <agarber@profectusgroup.com>
2020-03-08 14:20:14 +1100

    _exercise_files

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -6,16 +6,16 @@ import casual from 'casual';
 // Mongo connection
 var dbUrl = 'mongodb://localhost/friends';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/friends', {
+mongoose.connect( 'mongodb://localhost/friends', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
-  .then(() => console.log(`DB Connected at ${dbUrl}`))
-  .catch(err => {
-    console.log(`DB Connection Error: ${err.message}`)
-  });
+  } )
+  .then( () => console.log( `DB Connected at ${dbUrl}` ) )
+  .catch( err => {
+    console.log( `DB Connection Error: ${err.message}` )
+  } );
 
-const friendSchema = new mongoose.Schema({
+const friendSchema = new mongoose.Schema( {
   firstName: {
     type: String
   },
@@ -37,31 +37,41 @@ const friendSchema = new mongoose.Schema({
   contacts: {
     type: Array
   }
-});
+} );
 
-const Friends = mongoose.model('friends', friendSchema);
+const Friends = mongoose.model( 'friends', friendSchema );
 
-//SQL
-const sequelize = new Sequelize('database', null, null, {
+// SQL
+const sequelize = new Sequelize( 'database', null, null, {
   dialect: 'sqlite',
-  storage: '/aliens.sqlite'
-});
+  storage: './aliens.sqlite'
+} );
 
-const Aliens = sequelize.define('aliens', {
-  firstName: { type: Sequelize.STRING },
-  lastName: { type: Sequelize.STRING },
-  planet: { type: Sequelize.STRING }
-});
+const Aliens = sequelize.define( 'aliens', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  },
+  planet: {
+    type: Sequelize.STRING
+  }
+} );
 
-Aliens.sync({ force: true })
-  .then(() => {
-    _.times(10, (i) => {
-      Aliens.create({
-        firstName: casual.first_name,
-        lastName: casual.last_name,
-        planet: casual.word,
-      });
-    });
-  });
+Aliens.sync( {
+  force: true
+} ).then( () => {
+  _.times( 10, ( i ) => {
+    Aliens.create( {
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+      planet: casual.word,
+    } );
+  } );
+} );
 
-export { Friends, Aliens };
+export {
+  Friends,
+  Aliens
+};
